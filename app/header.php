6424cc7f66044529d58c2673f3b600fe5020b7f1
Marc Gugliuzza <marc.gugliuzza@gmail.com>
2013-06-29 13:06:41 -0400

    wording change

--- app/header.php
+++ app/header.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>BookSmash Project</title>
+    <title>BookLook Project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
@@ -15,7 +15,7 @@
   <div class="row-fluid">
     <div class="span2">
     	<!-- sidebar content -->
-		<h1>Book Smash</h1>
+		<h1>Book Look</h1>
     	<p class="lead">Smash up some books yo.</p>
     	<ul class="nav nav-pills nav-stacked">
     		<li <?php if(!isset($_POST["search_text"]) & !isset($_GET["page"])) echo " class=\"active\"";?>><a href="index.php">Home</a></li>
