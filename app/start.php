194a462320867f9aa7b00ead49901db2bd0c22b2
Marc Gugliuzza <marc.gugliuzza@gmail.com>
2013-06-30 21:14:01 -0400

    added start.php, first step towards discovery tool

---
+++ app/start.php
@@ -0,0 +1,22 @@
+<?php
+//start.php
+
+?>
+
+<!-- create a bootstrap grid. there are 10 cols in this div, so there should be 10 spans -->
+
+<div class="row">
+	
+	<div class="span5">
+		<img src="img/critic_picks.png" class="img-rounded" />
+		<div class="caption">Editor's Picks</div>
+	</div>
+
+	<div class="span5">
+	
+		<img src="img/magnifying_glass.png" class="img-rounded" />
+		
+		<div class="caption">Mystery</div>
+	</div>
+
+</div>
